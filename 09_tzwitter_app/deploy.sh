ae6c739c6f50cebe1f1a6b69481b8e05b3c48eed
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-07 13:06:50 +0100

    feat: deployment script
    Split the kernel, originate it, display a command to run a kernel

---
+++ 09_tzwitter_app/deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+account_alias=$1
+if [[ "$account_alias" == "" ]]; then echo "first argument should be an account alias with enough tez" && exit 1;fi
+
+# Compiling the kernel
+cargo build --release --target wasm32-unknown-unknown --manifest-path kernel/Cargo.toml
+
+# Deletes the previous rollup
+rm -rf rollup
+
+# Copy the kernel in the rollup directory
+mkdir -p rollup
+cp kernel/target/wasm32-unknown-unknown/release/kernel.wasm ./rollup
+
+# Installing the kernel
+wasm-strip ./rollup/kernel.wasm
+
+# Split it 
+KERNEL=$(xxd -ps -c 0 rollup/kernel.wasm | tr -d '\n')
+
+# Setup the DAC
+mkdir -p rollup/wasm_2_0_0
+mkdir -p /tmp/dac
+echo "{ \"reveal_data_dir\": \"$PWD/rollup/wasm_2_0_0\", \"mode\": { \"legacy\": true } }" > /tmp/dac/config.json
+
+# Run the DAC
+octez-dac-node run --data-dir /tmp/dac &> /dev/null&
+dac_pid=$!
+
+# Split the kernel
+sleep 3; # To be sure the dac is started
+ROOT_HASH=$(curl --silent --header "Content-Type: application/json" -X POST -d "{\"payload\":\"$KERNEL\", \"pagination_scheme\":\"Merkle_tree_V0\" }" http://localhost:10832/store_preimage | jq -r ".root_hash");
+kill $dac_pid
+
+# Installer kernel
+# To be faster we clone only one time the kernel repository
+if [ ! -d "/tmp/kernel" ]; then
+  git clone git@gitlab.com:tezos/kernel.git /tmp/kernel
+fi
+
+# Modify the line 41 of the installer_kernel (TODO: find a regex to do so, that does not implu )
+cd /tmp/kernel
+sed -i "41s/.*/b\"${ROOT_HASH}\";/" /tmp/kernel/installer_kernel/src/lib.rs
+cargo make wasm-preimage-installer 
+
+wasm-strip /tmp/kernel/target/wasm32-unknown-unknown/release/tezos_rollup_installer_kernel.wasm
+KERNEL_INSTALLER=$(xxd -ps -c 0 /tmp/kernel/target/wasm32-unknown-unknown/release/tezos_rollup_installer_kernel.wasm | tr -d '\n')
+cd -
+ls
+
+# Originate the kernel 
+SOR_ADDR=$(octez-client originate smart rollup from $account_alias \
+  of kind wasm_2_0_0 \
+  of type bytes \
+  with kernel "${KERNEL_INSTALLER}" \
+  --burn-cap 999 | grep "Address:" | awk '{print $2}')
+
+# # Setting up the rollup data directory
+octez-smart-rollup-node-alpha init operator config for "${SOR_ADDR}" with operators "$account_alias" --data-dir rollup
+
+# # Print the function to run it
+echo Rollup Address: $SOR_ADDR
+echo Command to run to start your rollup:
+echo "octez-smart-rollup-node-alpha run --data-dir rollup"
