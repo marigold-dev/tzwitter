8590e10cd7382d4d7c56c4d5f08fd82195c899b1
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-07 13:30:30 +0100

    init: front-end
    React application

---
+++ app/config-overrides.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  let loaders = config.resolve;
+  loaders.fallback = {
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer'),
+    os: require.resolve('os-browserify/browser'),
+  };
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  );
+
+  return config;
+};
---
+++ app/src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Index from './pages';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(<Index />);
