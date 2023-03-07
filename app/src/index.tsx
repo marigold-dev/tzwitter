c2e4f6000be33ecfb2dbf9ee0b8b8ed4182a760e
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-07 13:30:30 +0100

    init: front-end
    React application

---
+++ app/src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Index from './pages';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(<Index />);
+
