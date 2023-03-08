62ccbbd5ce9f672c603d745fd756de6696c3084a
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-08 14:29:18 +0100

    test: add a test to test the message

--- 09_tzwitter_app/kernel/src/lib.rs
+++ 09_tzwitter_app/kernel/src/lib.rs
@@ -53,3 +53,32 @@ fn entry<Host: RawRollupCore>(host: &mut Host) {
 }
 
 kernel_entry!(entry);
+
+#[cfg(test)]
+mod tests {
+    use mock_runtime::{host::MockHost, state::HostState};
+
+    use crate::{constants::MAGIC_BYTE, step};
+
+    fn valid_input() -> Vec<u8> {
+        let input = "7b22706b6579223a7b2245643235353139223a226564706b75444d556d375935337770346778654c425875694168585a724c6e385842315238336b737676657348384c7038626d43664b227d2c227369676e6174757265223a7b2245643235353139223a226564736967746658484337537875433378754453423563624a426a786b514672656f6e38584368526750446f674547355662506542545250794341513156586a75734e4a375537456557674d44703679634159473334774851665667726d47454a6974227d2c22696e6e6572223a7b226e6f6e6365223a312c22636f6e74656e74223a7b22506f73745477656574223a7b22617574686f72223a7b22547a31223a22747a315146443957714c575a6d6d4175716e6e545050556a666175697459455764736876227d2c22636f6e74656e74223a2248656c6c6f20776f726c64227d7d7d7d";
+        let msg = format!("01{:02x}{}", MAGIC_BYTE, input);
+
+        hex::decode(msg).unwrap()
+    }
+
+    #[test]
+    fn test_step() {
+        let state = HostState::default();
+        let input = valid_input();
+        let inputs = [input.as_slice()].into_iter();
+        let mut host = MockHost::from(state);
+
+        host.as_mut().set_ready_for_input(0);
+        host.as_mut().add_next_inputs(0, inputs);
+
+        let res = step(&mut host);
+
+        assert!(res.is_ok());
+    }
+}
