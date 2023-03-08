2a8ab3e3430c555a828b69e2bec88fedcd0c7f91
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-08 10:10:54 +0100

    refacto: change the definition of a message
    Also, changed the serialization, I prefer serde to have json
    capabilities

--- kernel/src/core/public_key_hash.rs
+++ kernel/src/core/public_key_hash.rs
@@ -1,9 +1,11 @@
 use crypto::hash::ContractTz1Hash;
+use serde::Deserialize;
 
 use crate::core::public_key::PublicKey;
 
 use super::hash::Blake2b20;
 
+#[derive(Deserialize)]
 pub enum PublicKeyHash {
     Tz1(ContractTz1Hash),
 }
