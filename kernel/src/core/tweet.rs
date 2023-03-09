336eaec9b144a9eab20cf2ed33b353c1eddffbc4
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-09 17:08:51 +0100

    bug: cannot create a tweet with same content
    Add a test to prevent this behaviour

--- kernel/src/core/tweet.rs
+++ kernel/src/core/tweet.rs
@@ -2,7 +2,7 @@ use serde::Serialize;
 
 use crate::core::public_key_hash::PublicKeyHash;
 
-use super::{hash::Blake2b, message::PostTweet};
+use super::message::PostTweet;
 
 #[derive(Serialize)]
 pub struct Tweet {
@@ -14,13 +14,5 @@ impl From<PostTweet> for Tweet {
     fn from(post_tweet: PostTweet) -> Self {
         let PostTweet { author, content } = post_tweet;
         Tweet { author, content }
-    }
-}
-
-impl Tweet {
-    /// Hash of the tweet
-    pub fn hash(&self) -> Blake2b {
-        let to_hash = format!("{}{}", self.author.to_b58(), &self.content);
-        Blake2b::from(to_hash.as_bytes())
     }
 }
