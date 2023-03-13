8dc8d406941f0a0763b200f9ba31c3102b5754dd
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-13 12:10:52 +0100

    refacto: client/account injections in pages

--- app/src/pages/account.tsx
+++ app/src/pages/account.tsx
@@ -1,24 +1,15 @@
 import './css/index.css';
-import { InMemorySigner } from '@taquito/signer';
-import { Tzwitter } from '../lib/tzwitter';
 import FeedContainer from '../containers/Feed';
 import { useNavigate, useParams } from 'react-router-dom';
 import FeedHeader from '../components/FeedHeader';
 import Menu from '../components/menu';
+import { Tzwitter } from '../lib/tzwitter';
 
-const secret = 'edsk3a5SDDdMWw3Q5hPiJwDXUosmZMTuKQkriPqY6UqtSfdLifpZbB';
-const signer = new InMemorySigner(secret);
-const TEZOS_URL = 'http://localhost:18731';
-const ROLLUP_URL = 'http://localhost:8932';
+interface AccountProperty {
+  tzwitter: Tzwitter;
+}
 
-const tzwitter = new Tzwitter({
-  signer,
-  tezosUrl: TEZOS_URL,
-  rollupUrl: ROLLUP_URL,
-  verbose: true,
-});
-
-const Account = () => {
+const Account = ({ tzwitter }: AccountProperty) => {
   const { publicKeyHash } = useParams() as { publicKeyHash: string };
   const navigate = useNavigate();
 
