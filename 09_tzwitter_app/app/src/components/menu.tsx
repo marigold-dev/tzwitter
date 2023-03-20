471e30736123c3082a75fd50254e3c2dede439d2
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-20 14:24:26 +0100

    refacto: menu and layout
    It's easier to add a menu entry to the application

--- 09_tzwitter_app/app/src/components/menu.tsx
+++ 09_tzwitter_app/app/src/components/menu.tsx
@@ -1,85 +1,37 @@
 import './Menu.css';
 
-type Entry = {
+export type MenuEntries = Array<{
   id: string;
-  className?: string;
-  logo: {
-    src: string;
-    width: string;
-    height: string;
-  };
-  text?: string;
   link: string;
-};
+  icon: string;
+  text: string;
+}>;
 
-const createMenu = (current: string): Array<Entry> =>
-  [
-    {
-      id: 'menu-tezos',
-      logo: {
-        src: '/tezos.png',
-        width: '30.54px',
-        height: '37.5px',
-      },
-      link: '/',
-    },
-    {
-      id: 'menu-home',
-      logo: {
-        src: '/home.svg',
-        width: '25px',
-        height: '25px',
-      },
-      text: 'Home',
-      link: '/',
-    },
-    {
-      id: 'menu-profile',
-      logo: {
-        src: '/profile.svg',
-        width: '25px',
-        height: '25px',
-      },
-      text: 'Profile',
-      link: `/profile`,
-    },
-  ].map((entry) => {
-    return entry.link === current
-      ? { className: 'menu-entry-bold', ...entry }
-      : entry;
-  });
-
-interface MenyType {
+interface MenuType {
   current: string;
   navigate: (path: string) => void;
+  menu: MenuEntries;
 }
 
-const Menu = ({ current, navigate }: MenyType) => {
-  const menuEntries = createMenu(current);
-
+const Menu = ({ menu, current, navigate }: MenuType) => {
   return (
     <div id="menu">
-      {menuEntries.map(
-        ({
-          id,
-          className,
-          logo: { src, width, height },
-          link,
-          text,
-        }: Entry) => {
-          return (
-            <div
-              key={id}
-              id={id}
-              className={className}
-              onClick={() => navigate(link)}
-            >
-              <img src={src} width={width} height={height} alt={id} />
-              {text ? <div>{text}</div> : <></>}
-            </div>
-          );
-        },
-      )}
+      <div id={'menu-tezos'} onClick={() => navigate('/')}>
+        <img
+          src={'/tezos.png'}
+          width={'30.54px'}
+          height={'37.5px'}
+          alt="tezos"
+        />
+      </div>
+      {menu.map(({ id, link, icon, text }) => (
+        <div key={id} className={'menu-entry'} onClick={() => navigate(link)}>
+          <img src={icon} width={'30.54px'} height={'37.5px'} alt={id} />
+          <div className={current === id ? 'menu-entry-bold' : undefined}>
+            {text}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
