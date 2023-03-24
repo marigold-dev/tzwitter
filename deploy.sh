f9e97934b814f3ef414f1905e65bbf227f22f39c
Pierre-Louis <pierrelouis.dubois@tutanota.com>
2023-03-24 11:11:38 +0100

    feat: FA2 contract
    This contract just has the storage of a a FA2 contract
    And exposes an entrypoint to mint a token

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,15 @@ which xxd > /dev/null || (echo "xxd should be installed" && exit 1)
 # wasm-strip
 which wasm-strip > /dev/null || (echo "wasm-strip should be installed" && echo "https://github.com/WebAssembly/wabt" exit 1)
 
+# ligo
+which ligo > /dev/null || (echo "wasm-strip should be installed" && echo "https://ligolang.org/docs/intro/installation?lang=jsligo" exit 1)
+
+# deploying the layer 1 contract
+MICHELSON=$(ligo compile contract smart_contract/dummy-fa2.jsligo)
+STORAGE=$(ligo compile storage smart_contract/dummy-fa2.jsligo initial_storage)
+
+export TZWITTER_L1_CONTRACT=$(octez-client originate contract tzwitter transferring 0 from $account_alias running "$MICHELSON" --init "$STORAGE" --burn-cap 1.0 --force | grep "New contract" | awk '{ print $3}')
+
 # Compiling the kernel
 cargo build --release --target wasm32-unknown-unknown --manifest-path kernel/Cargo.toml
 
@@ -26,7 +35,9 @@ cp kernel/target/wasm32-unknown-unknown/release/tzwitter_kernel.wasm ./rollup/ke
 # Installing the kernel
 wasm-strip ./rollup/kernel.wasm
 
-# Split it 
+# Using the smart-rollup-installer
+# It will generate the installer.hex
+# And split the kernel
 smart-rollup-installer get-reveal-installer --upgrade-to rollup/kernel.wasm --output rollup/installer.hex --preimages-dir rollup/wasm_2_0_0
 
 # Setup the DAC
--- simulate.sh
+++ simulate.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Required variable, otherwise the kernel won't compile
+export TZWITTER_L1_CONTRACT=KT1BN5u3CeRzVC7UaJYF4ADYtiHFHzqwC8dV
+
 # Build the kernel
 cargo build --release --target wasm32-unknown-unknown --manifest-path kernel/Cargo.toml
 
